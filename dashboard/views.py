f314e76c8be2e68e369b9ea2327531d7100fc090
Szymqn <koodziejskisz@outlook.com>
2025-08-22 22:50:46 +0200

    feat: add bookings' stats for dashboard, clear static change of cards

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,25 @@
+from django.db.models import Avg, F
 from django.shortcuts import render
+
+from bookings.models import Booking
 
 
 # Create your views here.
 def index(request):
-    return render(request, "dashboard/index.html")
+    total_bookings = Booking.objects.all().count()
+
+    avg_adr = Booking.objects.aggregate(Avg('adr'))['adr__avg']
+
+    avg_room_occupy = Booking.objects.annotate(
+        total_nights=F('stays_in_weekend_nights') + F('stays_in_week_nights')
+    ).aggregate(avg_total_nights=Avg('total_nights'))['avg_total_nights']
+
+    return render(
+        request,
+        "dashboard/index.html",
+        {
+            'total_bookings': total_bookings,
+            'avg_adr': round(avg_adr, 2),
+            'avg_room_occupy': round(avg_room_occupy, 2),
+        }
+    )
